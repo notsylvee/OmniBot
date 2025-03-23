7bb009a4f2bfb44db0ef6852e3a8e01a1a245e2d
Sylvee <79902941+notsylvee@users.noreply.github.com>
2025-03-23 18:09:03 -0400

    swearing doesnt make you cool v2

--- src/events/swearing.js
+++ src/events/swearing.js
@@ -6,7 +6,10 @@ module.exports = {
 
         if (message.author.bot || !message.guild) return;
         if (!message.guild.members.me.permissionsIn(message.channel.id).has(PermissionFlagsBits.SendMessages)) return;
-        if (!message.content.toLowerCase().includes("fuck" || "shit" || "bitch" || "ass")) return;
+        if (!message.content.toLowerCase().includes("fuck" || "shit" || "bitch" || "ass" || "hell")) return;
+
+        if (message.content.toLowerCase() === "fuck off i got this") {message.channel.send("Swearing doesnt make you cool")}
+        if (message.content.toLowerCase() === "fuck off, i got this") {message.channel.send("Swearing doesnt make you cool")}
 
         const chance = Math.random() * 250;
         if (chance < 245) {
