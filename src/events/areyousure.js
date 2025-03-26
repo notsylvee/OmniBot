5db04bb89d80312a4e58b824534d35f23db0e9a6
Sylvee <79902941+notsylvee@users.noreply.github.com>
2025-03-25 20:57:59 -0400

    made "are you sure?" rarer

--- src/events/areyousure.js
+++ src/events/areyousure.js
@@ -6,8 +6,8 @@ module.exports = {
 
         if (message.author.bot || !message.guild) return;
         if (!message.guild.members.me.permissionsIn(message.channel.id).has(PermissionFlagsBits.SendMessages)) return;
-        const chance = Math.random() * 300;
-        if (chance < 290) {
+        const chance = Math.random() * 400;
+        if (chance < 390) {
             return;
           } else {
             message.channel.send(`Are you sure?`);
