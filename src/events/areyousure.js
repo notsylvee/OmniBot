2974b5624fc2baf979801b85aa7b94bd872a0544
Sylvee <79902941+notsylvee@users.noreply.github.com>
2025-07-21 01:32:59 -0400

    possible fix for bot constantly breaking

--- src/events/areyousure.js
+++ src/events/areyousure.js
@@ -1,16 +1,16 @@
 const { PermissionFlagsBits } = require("discord.js");
 
 module.exports = {
-    name: "messageCreate",
-    async execute(message) {
+  name: "messageCreate",
+  async execute(message) {
 
-        if (message.author.bot || !message.guild) return;
-        if (!message.guild.members.me.permissionsIn(message.channel.id).has(PermissionFlagsBits.SendMessages)) return;
-        const chance = Math.random() * 400;
-        if (chance < 390) {
-            return;
-          } else {
-            message.channel.send(`Are you sure?`);
-          };
-    },
+    if (message.author.bot || !message.guild) return;
+
+    const chance = Math.random() * 400;
+    if (chance < 390) {
+      return;
+    } else {
+      message.channel.send(`Are you sure?`).catch((err) => {return});
+    };
+  },
 };
