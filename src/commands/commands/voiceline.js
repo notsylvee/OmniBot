3cf7c1f531b71415420e56f3f938a52903891ecc
Kyle <kb126027@student.wpsok.org>
2025-07-23 02:52:37 -0500

    Update voiceline.js
    I added more voicelines to omnibot for you

--- src/commands/commands/voiceline.js
+++ src/commands/commands/voiceline.js
@@ -18,7 +18,16 @@ module.exports = {
             "I wouldnt even keep you as a slave in my empire",
             "Look what they need to mimic a fraction of our power",
             "THINK MARK THINK",
-            "Im so lonely..."
+            "Im so lonely...",
+            "Let me break it down for you mark",
+            "SEA SALT! I NEED YOU SEA SALT!",
+            "Did you guys do this? That's not very nice." ,
+            "I think... I miss my wife.",
+            "I do love your mother but... she's more like a.. a pet to me.",
+            "Good news! We can finally be bees. This isn't your world, but we can be bees. This is good news! You can be a bee. You'll live like a bee. A pet. \nA pet? \nA pet. Mark, this is good news! You'll live for 30 years. \nThis is insane!!",
+            "Mark, look I made a steak. \nA STEAK!? \nYes.",
+            "Mark. Look I made us pizza! \n...What? \nI made us pizza! This is good news. Have some pizza! \nDon't touch me! \nCalm down. \nI will not calm down!! This isn't steak!!!!",
+            "Where's Mark, William."
         ];
         const voiceline = voicelines[Math.floor(Math.random() * voicelines.length)];
     
